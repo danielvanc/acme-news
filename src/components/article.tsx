c2c9d0b5666762e65553787c51745362259059e0
danielvanc <email@danielvanc.com>
2021-08-17 19:17:49 +0100

    Added claps and comments count

--- src/components/article.tsx
+++ src/components/article.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Link from 'next/link'
 
 export default function Article({ article }) {
+  const { id, title, claps_count, comments_count } = article;
   return (
     <div key={article.id}>
       <h2>
@@ -9,6 +10,10 @@ export default function Article({ article }) {
           <a>{article.title}</a>
         </Link>
       </h2>
+      <ul>
+        <li>{claps_count}👏🏼 </li>
+        <li>{comments_count > 0 ? comments_count : 0}💬 </li>
+      </ul>
     </div>
   )
 }
